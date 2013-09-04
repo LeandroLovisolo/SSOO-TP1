2b00f65c509e2c0879b09d87a758a92b005505d1
Lautaro Petaccio <lausuper@gmail.com>
2013-09-03 21:12:01 -0300

    SchedRR en reparacion

--- sched_rr.cpp
+++ sched_rr.cpp
@@ -9,10 +9,14 @@ using namespace std;
 SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
 	//Inicializo el vector con los quantum para cada core
-	for(int i = 1; i < argn[0]; ++i) {
+	cout << "Cores " << argn[0] << endl;
+	cout << "IDLE " << IDLE_TASK << endl;
+	for(int i = 1; i <= argn[0]; ++i) {
+		cout << "Qunatums " << argn[i] << endl;
 		quantumCore.push_back(argn[i]);
 		quantumRestanteCore.push_back(argn[i]);
 	}
+	cout << "Se crea" << endl;
 }
 
 SchedRR::~SchedRR() {
@@ -26,32 +30,41 @@ void SchedRR::load(int pid) {
 
 void SchedRR::unblock(int pid) {
 	tareasEnEspera.push(pid);
+	cout << "Tarea " << pid << " desbloqueada" << endl;
 }
 
 int SchedRR::tick(int cpu, const enum Motivo m) {
 	int tareaAEjecutar = IDLE_TASK;
 	if(m == EXIT || current_pid(cpu) == IDLE_TASK) {
 		if(!tareasEnEspera.empty()) {
-			tareaAEjecutar =  tareasEnEspera.front();
+			tareaAEjecutar = tareasEnEspera.front();
 			tareasEnEspera.pop();
 		}
 		quantumRestanteCore[cpu] = quantumCore[cpu];
 	}
-	else if(m == TICK || m == BLOCK) {
+	else if(m == TICK) {
 		//Si se le termino el quantum, cambio de tarea (si existe otra)
-		if(quantumRestanteCore[cpu] == 0 || m == BLOCK) {
+		if(quantumRestanteCore[cpu] == 0) {
 			if(!tareasEnEspera.empty()) {
 				tareaAEjecutar = tareasEnEspera.front();
 				tareasEnEspera.pop();
-				if(m == BLOCK) {
-					tareasEnEspera.push(current_pid(cpu));
-				}
+				tareasEnEspera.push(current_pid(cpu));
 			}
 			quantumRestanteCore[cpu] = quantumCore[cpu];
 		}
 		else {
 			quantumRestanteCore[cpu]--;
+			tareaAEjecutar = current_pid(cpu);
 		}
 	}
+	/*
+	else {
+		cout << "Tarea " << current_pid(cpu) << " bloqueada" << endl;
+		if(!tareasEnEspera.empty()) {
+			tareaAEjecutar = tareasEnEspera.front();
+			tareasEnEspera.pop();
+		}
+	}
+	*/
 	return tareaAEjecutar;
 }
