46b555562df255f1d80ee9f1b3b5cbe1eb1cefd8
Leandro Lovisolo <leandro@leandro.me>
2013-09-08 04:17:47 -0300

    Ejercicio 6

--- tasks.cpp
+++ tasks.cpp
@@ -20,11 +20,31 @@ void TaskAlterno(int pid, vector<int> params) { // params: ms_pid, ms_io, ms_pid
 	}
 }
 
-void TaskConsola(int pid, vector<int> params) { //params: n (llamadas), bmin y bmax (tiempo)
+void TaskConsola(int pid, vector<int> params) { // params: n (llamadas), bmin y bmax (tiempo)
 	srand(time(0));
 	for(int i=0; i<params[0]; i++) {
 		int duracion = rand() % (params[2]+1 - params[1]) + params[1];
 		uso_IO(pid, duracion);
+	}
+}
+
+void TaskBatch(int pid, vector<int> params) { // params: total_cpu, cant_bloqueos
+	// Tipos de operaciones: 0 = CPU, 1 = IO.
+	vector<int> operaciones(params[0], 0); // Inicializo con total_cpu operaciones de tipo CPU.
+
+	// Agrego cant_bloqueos operaciones de IO en posiciones al azar.
+	int bloqueos = 0;
+	while(bloqueos < params[1]) {
+		int i = rand() % params[0];
+		if(operaciones[i] == 0) {
+			operaciones[i] = 1;
+			bloqueos++;
+		}
+	}
+
+	for(size_t i = 0; i < operaciones.size(); i++) {
+		if(operaciones[i] == 0) uso_CPU(pid, 1);
+		else uso_IO(pid, 1);
 	}
 }
 
@@ -36,4 +56,5 @@ void tasks_init(void) {
 	register_task(TaskIO, 2);
 	register_task(TaskAlterno, -1);
 	register_task(TaskConsola, 3);
+	register_task(TaskBatch, 2);
 }
