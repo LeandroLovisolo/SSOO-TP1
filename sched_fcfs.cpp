003664d02444c5b485509e958f733bcb257613b3
Lautaro Petaccio <lausuper@gmail.com>
2013-09-09 09:49:30 -0300

    SchedFCFS ahora corre IDLE_TASK cuando la tarea se bloquea

--- sched_fcfs.cpp
+++ sched_fcfs.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <queue>
 #include "sched_fcfs.h"
+#include <iostream>
 
 using namespace std;
 
@@ -10,44 +11,47 @@ y no la suelta hasta terminarla. Cuando termina saca otra de la cola.
 
 // FCFS recibe la cantidad de cores.
 SchedFCFS::SchedFCFS(vector<int> argn) {
-	//Creo una lista para cada core
-	//tareaActivaCore = vector<int>(argn[0], IDLE_TASK);
+	for(int i=0; i < argn[0]; i++) {
+		tareaActualCore.push_back(tareaActual(Corriendo, IDLE_TASK));
+	}
 }
 
 SchedFCFS::~SchedFCFS() {
 }
 
 void SchedFCFS::load(int pid) {
-	//Recorro los cores para ver si tienen alguna tarea activa, si tienen
-	//pusheo la tarea a la cola, si no, la pongo como activa
-	/*
-	bool seEncolaTarea = true;
-	for(unsigned int i = 1; i < tareaActivaCore.size(); ++i) {
-		if(tareaActivaCore[i] == IDLE_TASK) {
-			tareaActivaCore[i] = pid;
-			seEncolaTarea = false;
-		}
-	}
-	if(seEncolaTarea) {
-		tareasEnEspera.push(pid);
-	}*/
 	tareasEnEspera.push(pid);
 }
 
 void SchedFCFS::unblock(int pid) {
-	//Se debe esperar hasta que se desbloquee, ya que es FIFO
+	tareaActualCore[tareasBloqueadas[pid]]._estado = Desbloqueandose;
 }
 
 int SchedFCFS::tick(int cpu, const enum Motivo m) {
 	int tareaAEjecutar = IDLE_TASK;
-	if(m == EXIT || current_pid(cpu) == IDLE_TASK) {
+	//Si la tarea terminó
+	//Si la tarea era IDLE_TASK y estaba corriendo, es decir, no había ninguna tarea
+	//y se agregó una nueva, cargarla.
+	if(m == EXIT || (current_pid(cpu) == IDLE_TASK && tareaActualCore[cpu]._estado == Corriendo)) {
 		if(!tareasEnEspera.empty()) {
 			tareaAEjecutar = tareasEnEspera.front();
 			tareasEnEspera.pop();
 		}
 	}
-	else if(m == BLOCK) {
-		tareaAEjecutar =  current_pid(cpu);
+	//La tarea está corriendo
+	else if(m == TICK && (tareaActualCore[cpu]._estado == Corriendo)) {
+		tareaAEjecutar = current_pid(cpu); //Seguir corriendola
+	}
+	//La tarea estuvo bloqueada, desbloquearla
+	else if(m == TICK && (tareaActualCore[cpu]._estado == Desbloqueandose)) {
+		tareaActualCore[cpu]._estado = Corriendo; //Cambio el estado de la tarea actual
+		tareaAEjecutar = tareaActualCore[cpu]._pid; //Pongo a ejecutar la tarea desbloqueada
+	}
+	else if(m == BLOCK){ //La tarea se bloqueó
+		tareaActualCore[cpu]._estado = Bloqueada; //La tarea actual pasa a estar bloqueada
+		tareaActualCore[cpu]._pid = current_pid(cpu); //Se guarda la tarea que se bloqueó
+		tareasBloqueadas[current_pid(cpu)] = cpu;
+		//Se deja la tarea IDLE_TASK mientras está bloqueada
 	}
 	return tareaAEjecutar;
 
--- sched_fcfs.h
+++ sched_fcfs.h
@@ -3,7 +3,19 @@
 
 #include <vector>
 #include <queue>
+#include <map>
 #include "basesched.h"
+
+enum Estado {Corriendo, Bloqueada, Desbloqueandose};
+
+struct tareaActual {
+	Estado _estado;
+	int _pid;
+	tareaActual(Estado estado, int pid) {
+		_estado = estado;
+		_pid = pid;
+	}
+};
 
 class SchedFCFS : public SchedBase {
 	public:
@@ -16,6 +28,9 @@ class SchedFCFS : public SchedBase {
 	private:
 		std::queue<int> tareasEnEspera;
 		std::vector<int> tareaActivaCore;
+		int pidTareaBloqueada;
+		std::vector<tareaActual> tareaActualCore;
+		std::map<int, int> tareasBloqueadas;
 };
 
 #endif
