6cd9b5f555e0bb6ffa7233ede338b073aa1a5211
Leandro Lovisolo <leandro@leandro.me>
2013-09-11 15:01:12 -0300

    Ejercicio 7: Resultados para tiempo de ejecución con 4 cores

--- ejercicio7/grafico_runtime_4_cores.m
+++ ejercicio7/grafico_runtime_4_cores.m
@@ -1,20 +1,20 @@
-function grafico_runtime_3_cores()
+function grafico_runtime_4_cores()
 
 % Obtengo matriz de runtimes
-R = runtimes(3);
+R = runtimes(4);
 
 % Rangos para el eje X
 X = {};
 for i = 1:8
-	X{end + 1} = strcat('$(', num2str(i), ', *, *)$');
+	X{end + 1} = strcat('$(', num2str(i), ', *, *, *)$');
 end
 
 % Rangos para el eje Y
 Ymin = ones(1, 8) * intmax();
 Ymax = zeros(1, 8);
 for i = 1:rows(R)
-	Ymin(R(i, 1)) = min(Ymin(R(i, 1)), R(i, 4));
-	Ymax(R(i, 1)) = max(Ymax(R(i, 1)), R(i, 4));
+	Ymin(R(i, 1)) = min(Ymin(R(i, 1)), R(i, 5));
+	Ymax(R(i, 1)) = max(Ymax(R(i, 1)), R(i, 5));
 end
 
 figure;
@@ -32,9 +32,9 @@ set(gca, 'xticklabel', X);
 legend({'M\''inimo', 'M\''aximo'});
 legend('boxon');
 
-ylim([38 45]);
+ylim([30 35]);
 
 oldpwd = pwd();
 cd('../tex');
-print('ej7-grafico-runtime-3-cores', '-S950,450', '-dtex');
+print('ej7-grafico-runtime-4-cores', '-S950,450', '-dtex');
 cd(oldpwd);
