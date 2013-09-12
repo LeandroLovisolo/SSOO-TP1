3f1bc5c19f96c888babacf62cf72e8a41f12a151
Nahuel <nahueldelgado@gmail.com>
2013-09-11 22:56:02 -0300

    Actualizado por las dudas.

--- scriptLot.sh
+++ scriptLot.sh
@@ -1,10 +1,10 @@
 ((promedio=0))
-for iter in {1..2000}
+for iter in {1..10000}
 do
 	((seed= $(rand) * $iter))
-	temp=$(./simusched ejercicio9/tareas.tsk 1 0 0 SchedLottery 10 $seed | grep EXIT | awk '{ print $2 }')
-	ultima=$(echo $temp | awk '{print $4}')
+	temp=$(./simusched ejercicio9/tareas.tsk 1 0 0 SchedLottery 2 $seed | grep EXIT | awk '{ print $2 }')
+	ultima=$(echo $temp | awk '{print $2}')
 	primera=$(echo $temp | awk '{print $1}')
-	((promedio= ($promedio*($iter-1) + $ultima - $primera)/$iter ))
+	promedio=$(echo "scale=2; ($promedio*($iter-1) + $ultima - $primera)/$iter" | bc)
 	echo $iter,$promedio
 done
